e181da110386f949165bb322e445f8c176262c61
oqrusk <oqrusk@gmail.com>
2017-12-30 12:03:48 +0900

    read from stdin

--- cat/cat.c
+++ cat/cat.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-static void do_cat(const char *path);
-static void die(const char *s);
+static void do_cat(FILE *f);
 
 int
 main(int argc, char *argv[])
 {
     int i;
-    
-    for (i = 1; i < argc; i++) {
-        FILE *f;
-        int c;
 
+    if (argc == 1) {
+        do_cat(stdin);
+    }
+    else{
+        for (i = 1; i < argc; i++) {
+        FILE *f;
         f = fopen(argv[i], "r");
 
         if (!f) {
             perror(argv[i]);
             exit(1);
         }
-        while ((c = fgetc(f)) != EOF) {
-            if (putchar(c) < 0) exit(1);
-        }
+        do_cat(f);
         fclose(f);
+        }
     }
+
     exit(0);
+}
+
+static void
+do_cat(FILE *f){
+    int c;
+
+    while ((c = fgetc(f)) != EOF ) {
+        if (putchar(c) < 0) exit(1);
+    }
 }
